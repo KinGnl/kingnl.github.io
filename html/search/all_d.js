363555fbf8a01da07372477e53e15226bebf2627
Nick GNONLONFOUN <KiNGnL@localhost.localdomain>
2022-11-13 19:56:48 +0100

    docs(r-type): hosting #4

--- html/search/all_d.js
+++ html/search/all_d.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['raylib_0',['Raylib',['../classRaylib.html',1,'Raylib'],['../classRaylib.html#a6912339ce0a1c189a7b756ad3ca1fe50',1,'Raylib::Raylib()']]],
-  ['receive_5fdata_1',['receive_data',['../classEnet.html#ab85cb2a871b7095762f4b8571c761a01',1,'Enet::receive_data()'],['../classnetwork_1_1INetworker.html#abf40ec464641221c4fa326404214efe6',1,'network::INetworker::receive_data()']]],
-  ['registry_2',['registry',['../classregistry.html',1,'']]],
-  ['room_5fid_3',['room_id',['../structcomponent_1_1room__id.html',1,'component']]],
-  ['room_5finfo_4',['room_info',['../structcomponent_1_1room__info.html',1,'component']]]
+  ['r_2dtype_20architecture_0',['R-TYPE Architecture',['../index.html',1,'']]],
+  ['raylib_1',['Raylib',['../classRaylib.html',1,'Raylib'],['../classRaylib.html#a6912339ce0a1c189a7b756ad3ca1fe50',1,'Raylib::Raylib()']]],
+  ['receive_5fdata_2',['receive_data',['../classEnet.html#ab85cb2a871b7095762f4b8571c761a01',1,'Enet::receive_data()'],['../classnetwork_1_1INetworker.html#abf40ec464641221c4fa326404214efe6',1,'network::INetworker::receive_data()']]],
+  ['registry_3',['registry',['../classregistry.html',1,'']]],
+  ['room_5fid_4',['room_id',['../structcomponent_1_1room__id.html',1,'component']]],
+  ['room_5finfo_5',['room_info',['../structcomponent_1_1room__info.html',1,'component']]]
 ];
